152351508fc8298b54ce6405e96b0d95b1ffe8e4
DzmitryGrom <Shelegeykodmitriy@mail.ru>
2016-07-24 00:15:41 +0300

    some

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,11 @@ module.exports = function (grunt) {
                     'css/select.css': 'less/select.less',
                     'css/service.css': 'less/service.less',
                     'css/service-all.css': 'less/service-all.less',
-                    'css/ready-work-service-open.css': 'less/ready-work-service-open.less'
+                    'css/ready-work-service-open.css': 'less/ready-work-service-open.less',
+                    'css/ready-work-service.css': 'less/ready-work-service.less',
+                    'css/ready-work.css': 'less/ready-work.less',
+                    'css/price.css': 'less/price.less',
+                    'css/conditions.css': 'less/conditions.less'
                 }
             }
         },
--- js/ready-work-service.js
+++ js/ready-work-service.js
@@ -1,6 +1,6 @@
-/*global $*/ 
+/*global $,jQuery*/
 $(document).ready(function () {
-    'use strict'; 
+    'use strict';
     //// Кнопки Pop-up
     $('.navbar-toggle').on("click", function () {
         $(".btn-togle-list").toggleClass("show");
@@ -62,101 +62,106 @@ $(document).ready(function () {
             itemEqualHeight: 414
         });
     });
+    
+    var sliderOne, sliderTwo, sliderFirst, sliderSecond;
+    
+    
     /// filter
-    if (screen.width > 768) {
-        var sliderOne = jQuery("#Slider3").slider({
+    if ($(window).width() > 768) {
+        sliderOne = jQuery("#Slider3").slider({
             from: 0,
             to: 40,
             scale: ['Любое', 'до 10', '20', '30', '40', 'более 45'],
             limits: false,
             step: 1,
             skin: "round_plastic"
-        }),
-            sliderTwo = jQuery("#Slider4").slider({
+        });
+        sliderTwo = jQuery("#Slider4").slider({
+            from: 0,
+            to: 30,
+            scale: ['Любое', 'до 5', '10', '20', '30', 'более 30'],
+            limits: false,
+            step: 1,
+            skin: "round_plastic"
+        });
+        sliderFirst = sliderOne.slider();
+        sliderSecond = sliderTwo.slider();
+    } else {
+        sliderOne = jQuery("#Slider3").slider({
+            from: 0,
+            to: 40,
+            scale: ['Любое', '20', 'более 40'],
+            limits: false,
+            step: 1,
+            skin: "round_plastic"
+        });
+        sliderTwo = jQuery("#Slider4").slider({
+            rom: 0,
+            to: 30,
+            scale: ['Любое', 'до 15', 'более 30'],
+            limits: false,
+            step: 1,
+            skin: "round_plastic"
+        });
+        sliderFirst = sliderOne.slider();
+        sliderSecond = sliderTwo.slider();
+    }
+    $(window).on('resize', function () {
+       
+        sliderOne.next().remove();
+        sliderTwo.next().remove();
+        if ($(window).width() < 768) {
+            sliderFirst.init(sliderOne, {
+                from: 0,
+                to: 40,
+                scale: ['Любое', '20', 'более 40'],
+                limits: false,
+                step: 1,
+                skin: "round_plastic"
+            });
+            sliderSecond.init(sliderTwo, {
+                from: 0,
+                to: 30,
+                scale: ['Любое', '15', 'более 30'],
+                limits: false,
+                step: 1,
+                skin: "round_plastic"
+            });
+        }
+        if ($(window).width() > 768) {
+            sliderFirst.init(sliderOne, {
+                from: 0,
+                to: 40,
+                scale: ['Любое', 'до 10', '20', '30', '40', 'более 40'],
+                limits: false,
+                step: 1,
+                skin: "round_plastic"
+            });
+            sliderSecond.init(sliderTwo, {
                 from: 0,
                 to: 30,
                 scale: ['Любое', 'до 5', '10', '20', '30', 'более 30'],
                 limits: false,
                 step: 1,
                 skin: "round_plastic"
-            }),
-            sliderFirst = sliderOne.slider(),
-            sliderSecond = sliderTwo.slider();
-    } else {
-        var sliderOne = jQuery("#Slider3").slider({
-                from: 0,
-                to: 40,
-                scale: ['Любое', '20', 'более 40'],
-                limits: false,
-                step: 1,
-                skin: "round_plastic",
-            }),
-            sliderTwo = jQuery("#Slider4").slider({
-                rom: 0,
-                to: 30,
-                scale: ['Любое', 'до 15', 'более 30'],
-                limits: false,
-                step: 1,
-                skin: "round_plastic"
-             })
-             , sliderFirst = sliderOne.slider()
-             , sliderSecond = sliderTwo.slider();
-     }
-     $(window).on('resize', function () {
-             sliderOne.next().remove();
-             sliderTwo.next().remove();
-             if (screen.width < 768) {
-                 sliderFirst.init(sliderOne, {
-                     from: 0
-                     , to: 40
-                     , scale: ['Любое', '20', 'более 40']
-                     , limits: false
-                     , step: 1
-                     , skin: "round_plastic"
-                 })
-                 sliderSecond.init(sliderTwo, {
-                     from: 0
-                     , to: 30
-                     , scale: ['Любое', '15', 'более 30']
-                     , limits: false
-                     , step: 1
-                     , skin: "round_plastic"
-                 })
-             }
-             if (screen.width > 768) {
-                 sliderFirst.init(sliderOne, {
-                     from: 0
-                     , to: 40
-                     , scale: ['Любое', 'до 10', '20', '30', '40', 'более 40']
-                     , limits: false
-                     , step: 1
-                     , skin: "round_plastic"
-                 })
-                 sliderSecond.init(sliderTwo, {
-                     from: 0
-                     , to: 30
-                     , scale: ['Любое', 'до 5', '10', '20', '30', 'более 30']
-                     , limits: false
-                     , step: 1
-                     , skin: "round_plastic"
-                 })
-             }
-         })
-         ////актив слайдера
-     $(".content-box-label-list-item_active-item").on("click", function () {
-             $(this).parent().find(".content-box-label-list-item_sub-item").css("display", "block");
-         })
-         /// Скрытие слайдера актив
-     $(document).mouseup(function (e) {
-         var toggleMenu = $(".content-box-label-list-item_sub-item");
-         if (!toggleMenu.is(e.target) && toggleMenu.has(e.target).length === 0) {
-             toggleMenu.css("display", "");
-         }
-     });
-     /// slider content-box-question
-     $(".regular").slick({
-         infinite: true
-         , slidesToShow: 1
-         , slidesToScroll: 1
-     });
- });
+            });
+        }
+    });
+    ////актив слайдера
+    $(".content-box-label-list-item_active-item").on("click", function () {
+        $(this).parent().find(".content-box-label-list-item_sub-item").css("display", "block");
+    });
+    /// Скрытие слайдера актив
+    $(document).mouseup(function (e) {
+        var toggleMenu = $(".content-box-label-list-item_sub-item");
+        if (!toggleMenu.is(e.target) && toggleMenu.has(e.target).length === 0) {
+            toggleMenu.css("display", "");
+        }
+    });
+    /// slider content-box-question
+    $(".regular").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
