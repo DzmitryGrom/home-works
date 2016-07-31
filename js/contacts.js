6bd84d9998d25be551cb6e55f2524eb07a808e70
DzmitryGrom <Shelegeykodmitriy@mail.ru>
2016-07-31 18:24:52 +0300

    add contacts page

--- js/contacts.js
+++ js/contacts.js
@@ -1,37 +1,34 @@
-/*global $*/
+/*global $, ymaps*/
 $(document).ready(function () {
     'use strict';
     
-   ymaps.ready(function () {
+    ymaps.ready(function () {
         var myMap = new ymaps.Map('map', {
-            center: [56.315695,44.017063],
+            center: [56.315695, 44.017063],
             zoom: 10,
             controls: []
-            },{
+        }, {
             suppressMapOpenBlock: true,
             searchControlProvider: 'yandex#search'
         }),
-        ButtonLayout = ymaps.templateLayoutFactory.createClass(
-            "<div class=map-pop-up><h2>{{data.header}}</h2><p><i class='fa fa-map-marker'></i><span class='map-pop-up-text-bottom-city'>{{data.city}}</span><i class='navbar-header-top_box-location-text fa fa-angle-down'></i>{{data.strit}}</p><p><span class='map-pop-up-text-bottom-number'><i class='fa fa-phone'></i>{{data.phoneOne}}</span><span class='map-pop-up-text-bottom-number'>{{data.phoneTwo}}</span></p><p><span><i class='fa fa-envelope'></i>{{data.mail}}</span></p><p><span><i class='fa fa-calendar-check-o'></i>{{data.work}}</span></p><p><span><i class='fa fa-clock-o'></i>{{data.time}}</span></p></div>"
-        ),            
-        button = new ymaps.control.Button({
-            data: {
-                content: "Жмак-жмак"
-            },
-            options: {
-                layout: ButtonLayout
-            }
-        }),  
-        placemark = new ymaps.Placemark(
-            [56.315695,44.017063],{
-            header: 'Работаем по все России',
-            city: 'г.Москва',
-            strit: 'ул.Красных Маевщиков 148а, строение 3, офис 298 (на углу возле "ТЦ Мир")',
-            phoneOne: '+7 (495) 777 55 77',
-            phoneTwo: '+7 (495) 777 55 77',
-            mail: 'example@mail.ru',
-            work: 'Понедельник - Воскресение',
-            time: 'с 09:00 до 00:00'
+            ButtonLayout = ymaps.templateLayoutFactory.createClass("<div class=map-pop-up><h2>{{data.header}}</h2><p><i class='fa fa-map-marker'></i><a href='#' class='btn-open-top-panel' onclick='$(\".top-panel\").css(\"transform\", \"translateY(0%)\")'><span class='map-pop-up-text-bottom-city'>{{data.city}}</span><i class='navbar-header-top_box-location-text fa fa-angle-down'></i></a>{{data.strit}}</p><p><span class='map-pop-up-text-bottom-number'><i class='fa fa-phone'></i>{{data.phoneOne}}</span><span class='map-pop-up-text-bottom-number'>{{data.phoneTwo}}</span></p><p><span><i class='fa fa-envelope'></i>{{data.mail}}</span></p><p><span><i class='fa fa-calendar-check-o'></i>{{data.work}}</span></p><p><span><i class='fa fa-clock-o'></i>{{data.time}}</span></p></div>"),
+            button = new ymaps.control.Button({
+                data: {
+                    content: "Жмак-жмак"
+                },
+                options: {
+                    layout: ButtonLayout
+                }
+            }),
+            placemark = new ymaps.Placemark([56.315695, 44.017063], {
+                header: 'Работаем по все России',
+                city: 'г.Москва',
+                strit: 'ул.Красных Маевщиков 148а, строение 3, офис 298 (на углу возле "ТЦ Мир")',
+                phoneOne: '+7 (495) 777 55 77',
+                phoneTwo: '+7 (495) 777 55 77',
+                mail: 'example@mail.ru',
+                work: 'Понедельник - Воскресение',
+                time: 'с 09:00 до 00:00'
             }, {
                 // Опции.
                 // Необходимо указать данный тип макета.
@@ -43,10 +40,7 @@ $(document).ready(function () {
                 // Смещение левого верхнего угла иконки относительно
                 // её "ножки" (точки привязки).
                 iconImageOffset: [-3, -42]
-        });
-        
-        
-
+            });
         placemark.events.add('click', function (e) {
             var placemark = e.get('target');
             button.data.set({
@@ -60,15 +54,10 @@ $(document).ready(function () {
                 time: placemark.properties.get('time'),
                 footer: placemark.properties.get('footer')
             });
-             $(".map-pop-up").toggleClass("show");
+            $(".map-pop-up").toggleClass("show");
         });
-        
-           
         myMap.geoObjects.add(placemark);
-        myMap.controls
-            .add(button, { position: { left: 0, top: 0 } })
-        
-
+        myMap.controls.add(button, { position: { left: 0, top: 0 } });
     });
     
    
